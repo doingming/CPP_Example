b83fc63598f8b596c4bce5a05a668278b57a7903
doming <domingfan@163.com>
2019-12-30 15:27:51 +0800

    Eigen first code

---
+++ Eigen/Eigen_define.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include "eigen3/Eigen/Dense"
+
+using namespace std;
+
+
+int main()
+{
+    Eigen::MatrixXd m(2,2);             //声明一个MatrixXd类型的变量，它是2*2的矩阵，未初始化
+    m(0,0) = 3;                         //将矩阵第1个元素初始化3
+    m(1,0) = 2.5;                       //将矩阵第3个元素初始化2.5
+    m(0,1) = -6;
+    m(1,1) = m(1, 0) + m(0, 1);
+    std::cout << m << std::endl;
+    
+    system("pause");
+    return 0;
+}
+
+
